170cfa3cf28f9156346294f1e5ae7098bfed6754
huangzhen <huangzhen@wzdai.com>
2018-04-13 10:51:37 +0800

    1
    1

--- DictionaryFiltered/ViewController.m
+++ DictionaryFiltered/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "NSDictionary+ParaProduct.h"
 
 @interface ViewController ()
 
@@ -16,7 +17,20 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    NSString *sss1 = @"生于成";
+    NSString *sss2 = @"";
+    NSString *sss3 = @"333";
+    NSString *sss4 = @"";
+    NSString *sss5 = @"让发";
+    NSNumber *sss6 = @(333);
+    NSString *sss9 = @"";
+    NSString *sss7 = nil;
+    NSString *sss8 = @"你好是 ";
+    
+    NSDictionary *dic = FilteredDictionaryOfVariableBindings(sss1,sss2,sss3,sss4,sss5,sss6,sss7,sss8,sss9,nil);
+    NSLog(@"%@",dic);
+    
 }
 
 
